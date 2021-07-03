efdb45697693ae486efa26adaccbdbeb1b477d93
Arose Niazi <arose.niazi@yahoo.com>
2021-07-04 03:34:34 +0500

    Completed

---
+++ ImprovedEular.js
@@ -0,0 +1,119 @@
+class ImprovedEular {
+    constructor(start, end, iAnswer, step, toShow) {
+
+        printHeading("Improved Eular Method", true, "h2");
+
+        this.start = math.fix(start,fixedLength);
+        this.end = math.fix(end,fixedLength);
+        this.h =  math.fix(step,fixedLength);
+        this.iAnswer =  math.fix(iAnswer,fixedLength);
+
+        printHeading("Starting Condition", true, "h3");
+        printHeading(`y(${this.start}) = ${this.iAnswer}`, true, "h3");
+        println(`t<sub>0</sub> = ${this.start}`);
+        println(`y<sub>0</sub> = ${this.iAnswer}`);
+        println(`h = ${this.h}`);
+
+        let predictorsEq = `y + h[${equation}]`;
+        let predictorsEqShow = `y<sub>n</sub> + h[${nEquation}]`;
+        let correctorEq = `y + (h/2)[${equation} + ${equation.replaceAll("x", "a").replaceAll("y", "b")}]`;
+        let correctorEqShow = `y<sub>n</sub> + h[${nEquation} + ${n1Equation.replaceAll("x", "<span class='topL'>x</span>").replaceAll("y", "<span class='topL'>y</span>")}]`;
+
+
+        let py = 0;
+        let y = this.iAnswer;
+        let px = 0;
+        let x = this.start;
+        for (let index = 0; index < toShow || toShow == 0; index++) {
+
+            printHeading("For n =" + index, true, "h3");
+            printHeading("Predictor", true, "h4");
+           
+            printHeading(`<span class='topL'>y</span><sub>n+1</sub>= ${predictorsEqShow}`, false, "h5");
+            printHeading(`<span class='topL'>y</span>${index + 1}</sub>= `+predictorsEqShow.replaceAll("n",`${index}`), false, "h5");
+            let pEqSolve = predictorsEq.replaceAll("y",`(${y})`).replaceAll("x", `(${x})`).replaceAll("h",`(${this.h})`);
+
+            printHeading(`<span class='topL'>y</span>${index + 1}</sub>= `+pEqSolve, false, "h5");
+            py =  math.fix(math.evaluate(pEqSolve.replaceAll("[", "(").replaceAll("]", ")")),fixedLength);
+            printHeading(`<span class='topL'>y</span>${index + 1}</sub>= `+py, false, "h5");
+
+            printHeading(`<span class='topL'>x</span><sub>n+1</sub>= x<sub>n</sub> + h`, false, "h5");
+            printHeading(`<span class='topL'>x</span><sub>${index + 1}</sub> x<sub>${index}</sub> + ${this.h}`, false, "h5");
+            printHeading(`<span class='topL'>x</span><sub>${index + 1}</sub>= ${x} + ${this.h}`, false, "h5");
+            px = math.fix(math.evaluate(`${x} + ${this.h}`), fixedLength);
+            printHeading(`<span class='topL'>x</span><sub>${index + 1}</sub>= ${px}`, false, "h5");
+
+
+            printHeading("Corrector", true, "h4");
+           
+            printHeading(`y<sub>n+1</sub>= ${correctorEqShow}`, false, "h5");
+            printHeading(`y${index + 1}</sub>= `+correctorEqShow.replaceAll("n+1",`${index + 1}`).replaceAll("n",`${index}`).replaceAll(`spa${index}`, "span"), false, "h5");
+            let cEqSolve = correctorEq.replaceAll("y",`(${y})`).replaceAll("x", `(${x})`).replaceAll("h",`(${this.h})`).replaceAll("a", `(${px})`).replaceAll("b",`(${py})`);
+
+            printHeading(`y${index + 1}</sub>= `+cEqSolve, false, "h5");
+            y =  math.fix(math.evaluate(cEqSolve.replaceAll("[", "(").replaceAll("]", ")")),fixedLength);
+            printHeading(`y${index + 1}</sub>= `+y, false, "h5");
+
+            printHeading(`x<sub>n+1</sub>= x<sub>n</sub> + h`, false, "h5");
+            printHeading(`x<sub>${index + 1}</sub>= x<sub>${index}</sub> + ${this.h}`, false, "h5");
+            printHeading(`x<sub>${index + 1}</sub>= ${x} + ${this.h}`, false, "h5");
+            x =  math.fix(math.evaluate(`${x} + ${this.h}`), fixedLength);
+            printHeading(`x<sub>${index + 1}</sub>= ${x}`, false, "h5");
+
+            if(x >= end) break;
+        }
+
+        py = 0;
+        y = this.iAnswer;
+        px = 0;
+        x = this.start;
+        createTable(["n", "x<sub>n</sub>", "y<sub>n</sub>", "<span class='topL'>x</span><sub>n+1</sub>", "<span class='topL'>y</span><sub>n+1</sub>", "x<sub>n+1</sub>", "x<sub>n+1</sub>"]);
+        for (let index = 0; x < end; index++) {
+
+            
+            let ox = x;
+            let oy = y;
+            let pEqSolve = predictorsEq.replaceAll("y",`(${y})`).replaceAll("x", `(${x})`).replaceAll("h",`(${this.h})`);
+            py =  math.fix(math.evaluate(pEqSolve.replaceAll("[", "(").replaceAll("]", ")")),fixedLength);
+            px = math.fix(math.evaluate(`${x} + ${this.h}`), fixedLength);
+            let cEqSolve = correctorEq.replaceAll("y",`(${y})`).replaceAll("x", `(${x})`).replaceAll("h",`(${this.h})`).replaceAll("a", `(${px})`).replaceAll("b",`(${py})`);
+
+            y =  math.fix(math.evaluate(cEqSolve.replaceAll("[", "(").replaceAll("]", ")")),fixedLength);
+            x =  math.fix(math.evaluate(`${x} + ${this.h}`), fixedLength);
+            addTableRow([index, ox, oy, px, py, x, y]);
+        }
+    }
+
+    find()
+    {
+        this.c = this.getNext();
+        this.e = this.relativeErrorFind();
+        let atC = equationSolve(this.c);
+        addTableRow([this.i++, this.start, this.a, this.end, this.b, this.c, atC, this.e])
+        if(rootResultChecker(atC))
+            return printHeading("Roots found at " + this.c, true, "h3");
+        if(this.b.includes(atC,0))
+            return printHeading("Roots found at " + this.a, true, "h3");
+
+        this.start = this.end;
+        this.a = this.b;
+        this.end = this.c;
+        this.b = atC;
+        this.oldc = this.c
+        this.find();
+    }
+
+    relativeErrorFind()
+    {
+        if(this.oldc == null) return "-";
+        let y = ((parseFloat(this.c) - parseFloat(this.oldc))/Math.abs(this.c));
+        return y.toFixed(fixedLength);
+    }
+
+    getNext()
+    {
+        let y = (parseFloat(this.end) - (((parseFloat(this.end) - parseFloat(this.start)) * this.b)/(this.b - this.a))).toFixed(fixedLength);
+        console.log("y =>" + y);
+        return y;
+    }
+}
---
+++ Runner.js
@@ -0,0 +1,94 @@
+//Variables to be used
+let fixedLength = 5;
+let a = b = c = d = e = f = 0;
+let scope;
+let equation;
+let nEquation;
+let n1Equation;
+let derivative;
+const allConstants = ["a","b", "c", "d", "f", "g"];
+let mymath;
+
+const keyValue = (input) => Object.entries(input).forEach(([key,value]) => {
+    println(key +" = "+ value);
+});
+
+function preformSettings(values, Precision, eq)
+{ 
+    scope = {};
+    if(values != "")
+    {
+        values =values.split(",");
+        for(let i = 0; i<values.length; i++)
+        {
+            scope[allConstants[i]] =  parseInt(values[i]);
+        }
+    }
+    
+    fixedLength = parseInt(Precision.value);
+    equation = eq.value;
+    console.log(scope);
+}
+
+function Solve(start, end, steps, Precision, values, eq, iAnswer, step)
+{
+    preformSettings(values.value, Precision, eq);
+    printHeading("Precision: 10<sup>-"+fixedLength+"</sup>",false, "h3");
+   
+    printHeading("Equation: "+math.parse(equation).toHTML(),false, "h3");
+    printHeading("Values: ",false, "h3");
+    keyValue(scope);
+
+    equation = math.simplify(equation,scope).toString();
+    printHeading("Simplified Equation: "+math.parse(equation).toHTML(),false, "h3");
+
+    printHeading("f(x,y) = "+math.parse(equation).toHTML(),false, "h3");
+    nEquation = equation.replaceAll("x","x<sub>n</sub>");
+    nEquation = nEquation.replaceAll("y","y<sub>n</sub>");
+    printHeading("f(x<sub>n</sub>,y<sub>n</sub>) = "+nEquation,false, "h3");
+    n1Equation = nEquation.replaceAll("n","n+1");
+    printHeading("f(x<sub>n+1</sub>,y<sub>n+1</sub>) = "+n1Equation,false, "h3");
+
+    if(parseInt(start.value) > parseInt(end.value))
+    {
+        let x = end;
+        end = start;
+        start = x;
+    }
+    new ImprovedEular(start.value, end.value, iAnswer.value, step.value, steps.value);
+
+    println("<hr>");
+}
+
+function equationSolve(x)
+{
+    return math.evaluate(equation, {x}).toFixed(fixedLength);  
+}
+
+function getDerivaite()
+{
+    return math.simplify(math.derivative(equation, "x")).toString();
+}
+
+function equationDerivativeSolve(x)
+{
+    return math.evaluate(derivative, {x}).toFixed(fixedLength);  
+}
+
+function clearOutput()
+{
+    document.getElementById("Output").innerHTML="";
+}
+
+function rootResultChecker(value)
+{
+    //console.log("before ->" + value);
+    value = value.toString().slice(0, value.toString().length - (fixedLength - toCheckLength));
+    //console.log("after ->" + value);
+    value = parseFloat(value);
+
+    if(!(value < 0 || value > 0))
+        return true;
+    return false;
+}
+
